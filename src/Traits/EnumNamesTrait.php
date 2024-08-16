493dd190ca73e5e406bfc7c60e78775798c57da0
Baspa <hello@baspa.dev>
2024-08-16 15:08:55 +0200

    Remove classes

--- src/Traits/EnumNamesTrait.php
+++ src/Traits/EnumNamesTrait.php
@@ -4,8 +4,6 @@ namespace Vormkracht10\TwoFactorAuth\Traits;
 
 trait EnumNamesTrait
 {
-    abstract public static function cases(): array;
-
     public static function names(): array
     {
         return array_map(fn ($enum) => $enum->name, static::cases());
--- src/Traits/EnumValuesTrait.php
+++ src/Traits/EnumValuesTrait.php
@@ -4,8 +4,6 @@ namespace Vormkracht10\TwoFactorAuth\Traits;
 
 trait EnumValuesTrait
 {
-    abstract public static function cases(): array;
-
     public static function values(): array
     {
         return array_map(fn ($enum) => $enum->value, static::cases());
