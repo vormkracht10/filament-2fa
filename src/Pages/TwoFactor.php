5681770ebcc91f1ee9855152eb4068aa7f15f782
Baspa <hello@baspa.dev>
2024-09-19 21:05:02 +0200

    [Fix] Only show deactive notification when actually deactivated

--- src/Pages/TwoFactor.php
+++ src/Pages/TwoFactor.php
@@ -275,12 +275,14 @@ class TwoFactor extends Page implements HasForms
     {
         $disable($this->user);
 
-        Notification::make()
-            ->title(__('Two-Factor Authentication deactivated'))
-            ->body(__('You can now log in without a code.'))
-            ->success()
-            ->duration(5000)
-            ->send();
+        if ($this->user->two_factor_confirmed_at) {
+            Notification::make()
+                ->title(__('Two-Factor Authentication deactivated'))
+                ->body(__('You can now log in without a code.'))
+                ->success()
+                ->duration(5000)
+                ->send();
+        }
 
         $this->showingQrCode = false;
         $this->showingConfirmation = false;
