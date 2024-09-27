12dbd94fb5469c7310ba5143d9c9893ad2dac285
Baspa <hello@baspa.dev>
2024-09-27 13:41:30 +0200

    Remove component

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,9 @@
-<x-filament::layouts.card title="{{__('Verify')}}">
-    <div class="filament-fortify-verify-email-page">
-        <form method="POST" action="{{ route('verification.send') }}" class="space-y-8">
+<div class="filament-fortify-verify-email-page">
+    <form method="POST" action="{{ route('verification.send') }}" class="space-y-8">
 
-            @csrf
-            <x-filament::button type="submit" class="w-full">
-                {{ __('Verify') }}
-            </x-filament::button>
-        </form>
-    </div>
-</x-filament::layouts.card>
+        @csrf
+        <x-filament::button type="submit" class="w-full">
+            {{ __('Verify') }}
+        </x-filament::button>
+    </form>
+</div>
