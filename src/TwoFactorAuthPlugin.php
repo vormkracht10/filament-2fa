75be0ba4dabbb1fa26335c0fd4222e947714139a
markvaneijk <markvaneijk@users.noreply.github.com>
2024-10-22 12:41:22 +0000

    Fix styling

--- src/TwoFactorAuthPlugin.php
+++ src/TwoFactorAuthPlugin.php
@@ -42,7 +42,7 @@ class TwoFactorAuthPlugin implements Plugin
                 'two-factor-authentication' => MenuItem::make()
                     ->icon('heroicon-o-lock-closed')
                     ->label(__('Two-Factor Authentication'))
-                    ->url(fn(): string => TwoFactor::getUrl()),
+                    ->url(fn (): string => TwoFactor::getUrl()),
             ]);
         }
 
