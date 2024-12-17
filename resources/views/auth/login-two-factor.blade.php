cdb6f16ee1d8e8b8da2d3198a176aae687eefd48
Baspa <hello@baspa.dev>
2024-12-17 12:51:51 +0100

    Fix primary color on login two factor page

--- resources/views/auth/login-two-factor.blade.php
+++ resources/views/auth/login-two-factor.blade.php
@@ -17,7 +17,7 @@
         {{ $this->form }}
 
         <div class="flex items-center justify-between mt-6">
-            <x-filament::button type="submit" class="w-full" color="primary">
+            <x-filament::button type="submit" class="w-full bg-primary-500">
                 {{ __('Login') }}
             </x-filament::button>
         </div>
