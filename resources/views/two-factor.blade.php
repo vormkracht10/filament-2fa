e4781b5581eec2284335f77309aa3ddf8d708961
Bas van Dinther <hello@baspa.dev>
2025-01-21 08:45:31 +0100

    [Fix] Width of 2FA auth page (#77)

--- resources/views/two-factor.blade.php
+++ resources/views/two-factor.blade.php
@@ -40,68 +40,66 @@
 
                     @if ($this->showTwoFactor())
                         <div class="px-4 py-6 sm:p-8">
-                            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                                <div class="sm:col-span-6">
-                                    @if ($this->showQrCode)
-                                        <div class="text-center space-y-8 mb-4">
-                                            <div>
-                                                @unless ($showingQrCode)
-                                                    <div class="font-bold">
-                                                        {!! __('Two-Factor Authentication enabled') !!}
+                            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8">
+                                @if ($this->showQrCode)
+                                    <div class="text-center space-y-8 mb-4">
+                                        <div>
+                                            @unless ($showingQrCode)
+                                                <div class="font-bold">
+                                                    {!! __('Two-Factor Authentication enabled') !!}
+                                                </div>
+                                            @else
+                                                <div class="font-bold">
+                                                    {!! __('Or scan the QR code with your authenticator app') !!}.
+                                                </div>
+                                                <div class="flex items-center justify-center mt-2">
+                                                    <div class="border-4 border-white">
+                                                        {!! $user->twoFactorQrCodeSvg() !!}
                                                     </div>
-                                                @else
-                                                    <div class="font-bold">
-                                                        {!! __('Or scan the QR code with your authenticator app') !!}.
-                                                    </div>
-                                                    <div class="flex items-center justify-center mt-2">
-                                                        <div class="border-4 border-white">
-                                                            {!! $user->twoFactorQrCodeSvg() !!}
-                                                        </div>
-                                                    </div>
-                                                    <br />
-                                                    <p class="text-sm">
-                                                        {!! __('The secret key to setup the authenticator app is') !!}: <br />
-                                                        <span
-                                                            class="font-bold mt-4">{{ decrypt($user->two_factor_secret) }}</span>
-                                                    </p>
-                                                @endunless
+                                                </div>
+                                                <br />
+                                                <p class="text-sm">
+                                                    {!! __('The secret key to setup the authenticator app is') !!}: <br />
+                                                    <span
+                                                        class="font-bold mt-4">{{ decrypt($user->two_factor_secret) }}</span>
+                                                </p>
+                                            @endunless
+                                        </div>
+                                    </div>
+                                @endif
+
+                                @if ($showingRecoveryCodes)
+                                    <div class="text-center text-sm">
+                                        {!! __(
+                                            'Save these recovery codes in a secure place as they can be used to recover access to your account if you lose your device',
+                                        ) !!}.
+                                        <div class="flex items-center justify-center">
+                                            <div class="mt-2 text-left text-sm">
+                                                @foreach ((array) $user->recoveryCodes() as $index => $code)
+                                                    <p class="mt-2">{{ $code }}</p>
+                                                @endforeach
                                             </div>
                                         </div>
+                                    </div>
+                                @endif
+
+                                @if ($showingConfirmation)
+                                    <div class="mt-4">
+                                        {{ $this->otpCodeForm }}
+                                    </div>
+                                @endif
+
+                                <div class="mt-6 flex items-center justify-end gap-x-6">
+                                    @if (!$showingRecoveryCodes && !$user->two_factor_confirmed_at)
+                                        {{ $this->disableAction() }}
                                     @endif
 
                                     @if ($showingRecoveryCodes)
-                                        <div class="text-center text-sm">
-                                            {!! __(
-                                                'Save these recovery codes in a secure place as they can be used to recover access to your account if you lose your device',
-                                            ) !!}.
-                                            <div class="flex items-center justify-center">
-                                                <div class="mt-2 text-left text-sm">
-                                                    @foreach ((array) $user->recoveryCodes() as $index => $code)
-                                                        <p class="mt-2">{{ $code }}</p>
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                        </div>
+                                        {{ $this->downloadAction() }}
+                                        {{ $this->regenerateAction() }}
+                                    @elseif ($showingConfirmation)
+                                        {{ $this->confirmAction() }}
                                     @endif
-
-                                    @if ($showingConfirmation)
-                                        <div class="mt-4">
-                                            {{ $this->otpCodeForm }}
-                                        </div>
-                                    @endif
-
-                                    <div class="mt-6 flex items-center justify-end gap-x-6">
-                                        @if (!$showingRecoveryCodes && !$user->two_factor_confirmed_at)
-                                            {{ $this->disableAction() }}
-                                        @endif
-
-                                        @if ($showingRecoveryCodes)
-                                            {{ $this->downloadAction() }}
-                                            {{ $this->regenerateAction() }}
-                                        @elseif ($showingConfirmation)
-                                            {{ $this->confirmAction() }}
-                                        @endif
-                                    </div>
                                 </div>
                             </div>
                         </div>
